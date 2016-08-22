7d9dfe44477a359b2c79842760c98fffb3f21b60
Nick <nicholas.kundu@gmail.com>
2016-08-22 00:57:28 +0000

    data is sent to filesystem

--- native/readTemp.sh
+++ native/readTemp.sh
@@ -4,8 +4,7 @@ OUTPUT=$(sudo ./dht22)
 HUMID=$(echo "$OUTPUT" | head -n 1 | tr -d '\n')
 TEMP=$(echo "$OUTPUT" | tail -n 1 | tr -d '\n')
 NOW=$(date +"%Y-%m-%d %H:%M")
-echo $NOW
-echo $HUMID
-echo $TEMP
+echo "$NOW"",""$HUMID" >> ~/monitors/data/inputs/humid1/data.csv
+echo "$NOW"",""$TEMP" >> ~/monitors/data/inputs/temp1/data.csv
 sleep 60
 done
