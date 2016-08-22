cc05ebf74cec4ce5b46964e9ae35a31dc3c42043
Nick Kundu <nicholas.kundu@gmail.com>
2016-08-21 20:19:13 -0400

    spinner

---
+++ public/js/my.js
@@ -0,0 +1,20 @@
+var showBusyModal = function () {
+    $.blockUI({ 
+    	message: '<div class="spinner"></div><br />Please wait...',
+    	css: { 
+        border: 'none', 
+        padding: '15px', 
+        backgroundColor: '#000', 
+        '-webkit-border-radius': '10px', 
+        '-moz-border-radius': '10px', 
+        opacity: .5, 
+        color: '#fff' 
+      }
+    }); 
+}
+
+$(document).ready(function(){
+​  $('a').click(function(){
+     $('<div class=loadingDiv>loading...</div>').prependTo(document.body); 
+  });​
+});
